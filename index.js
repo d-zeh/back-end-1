c53c863e411fe412f2080c052a1f12a646acbbf8
Daniel Zeh <dfzeh1@gmail.com>
2024-01-19 20:58:22 -0600

    "commit"

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.use(express.json());
 
 app.listen(4000, () => console.log("Server running on 5500"));
 
-app.get('/api/users' (req,res) => {
-    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
-    res.status(200).send(friends)
+app.get('/api/users', (req,res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"]
+    
+    res.status(200).send(friends);
 
 })
